02c9a667a817ae042e7e41a37fc51b060ca4fc6d
Samuel Hassine <samuel.hassine@anotherservice.com>
2013-12-02 16:33:46 +0100

    Disable register

--- as/join_action.php
+++ as/join_action.php
@@ -14,7 +14,7 @@ if( isset($_POST['antispam']) && $_POST['antispam'] == $_SESSION['ANTISPAM'] )
 		$_SESSION['JOIN_USER'] = $_POST['username'];
 		$_SESSION['JOIN_EMAIL'] = $_POST['email'];
 		
-		$result = api::send('registration/add', array('auth'=>'', 'user'=>$_POST['username'], 'email'=>$_POST['email'], 'invitation'=>$_POST['code']), $GLOBALS['CONFIG']['API_USERNAME'].':'.$GLOBALS['CONFIG']['API_PASSWORD']);
+		//$result = api::send('registration/add', array('auth'=>'', 'user'=>$_POST['username'], 'email'=>$_POST['email'], 'invitation'=>$_POST['code']), $GLOBALS['CONFIG']['API_USERNAME'].':'.$GLOBALS['CONFIG']['API_PASSWORD']);
 		
 		$email = str_replace(array('{USER}', '{EMAIL}', '{CODE}'), array($_POST['username'], $_POST['email'], $result['code']), $lang['content']);
 		$result = mail($_POST['email'], $lang['subject'], str_replace(array('{TITLE}', '{CONTENT}'), array($lang['email_title'], $email), $GLOBALS['CONFIG']['MAIL_TEMPLATE']), "MIME-Version: 1.0\r\nContent-type: text/html; charset=utf-8\r\nFrom: Another Service <no-reply@anotherservice.com>\r\n");

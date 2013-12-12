5a574c3a99ef83b35291f03592692793976213ad
Samuel Hassine <samuel.hassine@anotherservice.com>
2013-12-12 07:46:16 +0100

    Bugfix

--- as/panel/app/instance_less_action.php
+++ as/panel/app/instance_less_action.php
@@ -12,7 +12,7 @@ $app = $app[0];
 if( count($app['instances'])-1 == 0 )
 	$template->redirect('/panel/app/show?id=' . $_GET['id']);
 
-api::send('self/app/update', array('app'=>$_GET['id'], 'branch'=>$_GET['branch'], 'instances' => count($app['instances'])-1));
+api::send('self/app/update', array('app'=>$_GET['id'], 'branch'=>$_GET['branch'], 'instances' => count($app['branches'][$_GET['branch']]['instances'])-1));
 
 if( isset($_GET['redirect']) )
 	template::redirect($_GET['redirect']);
--- as/panel/app/instance_plus_action.php
+++ as/panel/app/instance_plus_action.php
@@ -9,7 +9,7 @@ if( !defined('PROPER_START') )
 $app = api::send('self/app/list', array('id'=>$_GET['id'], 'extended'=>1));
 $app = $app[0];
 
-api::send('self/app/update', array('app'=>$_GET['id'], 'branch'=>$_GET['branch'], 'instances' => count($app['instances'])+1));
+api::send('self/app/update', array('app'=>$_GET['id'], 'branch'=>$_GET['branch'], 'instances' => count($app['branches'][$_GET['branch']]['instances'])+1));
 
 if( isset($_GET['redirect']) )
 	template::redirect($_GET['redirect']);

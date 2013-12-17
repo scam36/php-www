a6daeee7cfb404dd2266506925478569cb9af22b
Samuel Hassine <samuel.hassine@anotherservice.com>
2013-12-17 22:19:34 +0100

    Bugfix

--- on/confirm_action.php
+++ on/confirm_action.php
@@ -8,20 +8,32 @@ if( !defined('PROPER_START') )
 
 try
 {
-	$result = api::send('user/add', array('user'=>$_POST['username'], 'pass'=>$_POST['password'], 'email'=>$_POST['email'], 'ip'=>$_SERVER['HTTP_X_REAL_IP'], 'firstname'=>'', 'lastname'=>'', 'language'=>translator::getLanguage()), $GLOBALS['CONFIG']['API_USERNAME'].':'.$GLOBALS['CONFIG']['API_PASSWORD']);
+	$result = api::send('user/add', array('user'=>$_GET['user'], 'ip'=>$_SERVER['HTTP_X_REAL_IP'], 'pass'=>$_POST['password'], 'email'=>$_GET['email'], 'firstname'=>'', 'lastname'=>'', 'language'=>translator::getLanguage()), $GLOBALS['CONFIG']['API_USERNAME'].':'.$GLOBALS['CONFIG']['API_PASSWORD']);
 	$uid = $result['id'];
-	
+
+	if( !is_numeric($uid) || $uid <= 0 )
+		throw new SiteException("User creation failed", 500, "Generated uid : {$uid}");
+
+	// REGISTRATION IS OK -> DELETE REGISTRATION
+	api::send('registration/delete', array('user'=>$_POST['username']), $GLOBALS['CONFIG']['API_USERNAME'].':'.$GLOBALS['CONFIG']['API_PASSWORD']);
+
+	// INSERT THE USER IN THE USERS GROUP
 	api::send('group/user/add', array('user'=>$uid, 'group'=>'USERS'), $GLOBALS['CONFIG']['API_USERNAME'].':'.$GLOBALS['CONFIG']['API_PASSWORD']);
 
-	$result = api::send('token/insert', array('user'=>$uid, 'lease'=>0, 'name'=>'Bus IT', 'grants'=>'ACCESS,SELF_ACCESS,CONNECTOR_SELECT,SELF_LINK_INSERT,SELF_LINK_UPDATE,SELF_LINK_SELECT,SELF_LINK_DELETE,SELF_BILL_INSERT,SELF_BILL_UPDATE,SELF_BILL_DELETE,SELF_BILL_SELECT,SELF_GRANT_TOKEN_DELETE,SELF_GRANT_TOKEN_INSERT,SELF_GRANT_TOKEN_SELECT,SELF_GRANT_USER_SELECT,SELF_GROUP_USER_DELETE,SELF_GROUP_USER_SELECT,SELF_IDENTITY_DELETE,SELF_IDENTITY_INSERT,SELF_IDENTITY_SELECT,SELF_IDENTITY_UPDATE,SELF_INSTANCE_DELETE,SELF_INSTANCE_INSERT,SELF_INSTANCE_SELECT,SELF_INSTANCE_UPDATE,SELF_QUOTA_USER_SELECT,SELF_TOKEN_DELETE,SELF_TOKEN_INSERT,SELF_TOKEN_SELECT,SELF_TOKEN_UPDATE,SELF_USER_DELETE,SELF_USER_SELECT,SELF_USER_UPDATE,SELF_GRANT_TOKEN_DELETE,SELF_GRANT_TOKEN_INSERT,SELF_GRANT_TOKEN_SELECT,SELF_GROUP_USER_DELETE,SELF_GROUP_USER_SELECT,SELF_QUOTA_USER_SELECT,SELF_TOKEN_DELETE,SELF_TOKEN_INSERT,SELF_TOKEN_SELECT,SELF_TOKEN_UPDATE,SELF_USER_DELETE,SELF_USER_SELECT,SELF_USER_UPDATE,SELF_SPACE_SELECT,SELF_SPACE_UPDATE,SELF_SPACE_DELETE,SELF_SPACE_INSERT'), $GLOBALS['CONFIG']['API_USERNAME'].':'.$GLOBALS['CONFIG']['API_PASSWORD']);
-	$token = $result['value'];
+	// CREATE THE FIRST TOKEN WITH BASIC ACCESS
+	$result = api::send('token/insert', array('user'=>$uid, 'lease'=>0, 'name'=>'Olympe', 'grants'=>'ACCESS,SELF_SELECT,SELF_UPDATE,SELF_DELETE,SELF_GRANT_SELECT,SELF_GROUP_SELECT,SELF_GROUP_DELETE,SELF_TOKEN_INSERT,SELF_TOKEN_SELECT,SELF_TOKEN_UPDATE,SELF_TOKEN_DELETE,SELF_QUOTA_SELECT,SELF_TOKEN_GRANT_DELETE,SELF_TOKEN_GRANT_INSERT,SELF_SITE_SELECT,SELF_SITE_DELETE,SELF_SITE_INSERT,SELF_SITE_UPDATE,SELF_DOMAIN_INSERT,SELF_DOMAIN_SELECT,SELF_DOMAIN_DELETE,SELF_DOMAIN_UPDATE,SELF_DATABASE_INSERT,SELF_DATABASE_UPDATE,SELF_DATABASE_DELETE,SELF_DATABASE_SELECT,SELF_SUBDOMAIN_SELECT,SELF_SUBDOMAIN_UPDATE,SELF_SUBDOMAIN_INSERT,SELF_SUBDOMAIN_DELETE,SELF_ACCOUNT_DELETE,SELF_ACCOUNT_INSERT,SELF_ACCOUNT_SELECT,SELF_ACCOUNT_UPDATE,SELF_APP_INSERT,SELF_APP_DELETE,SELF_APP_UPDATE,SELF_APP_SELECT'), $GLOBALS['CONFIG']['API_USERNAME'].':'.$GLOBALS['CONFIG']['API_PASSWORD']);
+	$token = $result['token'];
 	
-	api::send('quota/user/add', array('user'=>$uid, 'quotas'=>'CREDITS'), $GLOBALS['CONFIG']['API_USERNAME'].':'.$GLOBALS['CONFIG']['API_PASSWORD']);
+	// ADD USER QUOTAS
+	api::send('quota/user/add', array('user'=>$uid, 'quotas'=>'SITES,DATABASES,DOMAINS,BYTES,MAILS'), $GLOBALS['CONFIG']['API_USERNAME'].':'.$GLOBALS['CONFIG']['API_PASSWORD']);
+	api::send('quota/user/update', array('user'=>$uid, 'quota'=>'SITES', 'max'=>1), $GLOBALS['CONFIG']['API_USERNAME'].':'.$GLOBALS['CONFIG']['API_PASSWORD']);
+	api::send('quota/user/update', array('user'=>$uid, 'quota'=>'DATABASES', 'max'=>3), $GLOBALS['CONFIG']['API_USERNAME'].':'.$GLOBALS['CONFIG']['API_PASSWORD']);
+	api::send('quota/user/update', array('user'=>$uid, 'quota'=>'DOMAINS', 'max'=>3), $GLOBALS['CONFIG']['API_USERNAME'].':'.$GLOBALS['CONFIG']['API_PASSWORD']);
+	api::send('quota/user/update', array('user'=>$uid, 'quota'=>'BYTES', 'max'=>200), $GLOBALS['CONFIG']['API_USERNAME'].':'.$GLOBALS['CONFIG']['API_PASSWORD']);
+	api::send('quota/user/update', array('user'=>$uid, 'quota'=>'MAILS', 'max'=>100), $GLOBALS['CONFIG']['API_USERNAME'].':'.$GLOBALS['CONFIG']['API_PASSWORD']);
 	
 	$email = str_replace(array('{EMAIL}', '{USER}', '{PASS}'), array($_POST['email'], $_POST['username'], $_POST['password']), $lang['content']);
-	$result = mail($_POST['email'], $lang['subject'], str_replace('{CONTENT}', $email, $GLOBALS['CONFIG']['MAIL_TEMPLATE']), "MIME-Version: 1.0\r\nContent-type: text/html; charset=utf-8\r\nFrom: Bus IT <no-reply@bus-it.com>\r\Bcc: contact@bus-it.com\r\n");
-
-	api::send('confirm/delete', array('id'=>$_POST['id']), $GLOBALS['CONFIG']['API_USERNAME'].':'.$GLOBALS['CONFIG']['API_PASSWORD']);
+	$result = mail($_POST['email'], $lang['subject'], str_replace('{CONTENT}', $email, $GLOBALS['CONFIG']['MAIL_TEMPLATE']), "MIME-Version: 1.0\r\nContent-type: text/html; charset=utf-8\r\nFrom: Olympe <no-reply@olympe.in>\r\Bcc: contact@olympe.in\r\n");
 	
 	$_SESSION['MESSAGE']['TYPE'] = 'success';
 	$_SESSION['MESSAGE']['TEXT']= $lang['success'];

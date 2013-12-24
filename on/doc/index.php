9387db25bd6d822925412762655f5d6970565883
Samuel Hassine <samuel.hassine@anotherservice.com>
2013-12-24 02:55:45 +0100

    Starting infra

--- on/doc/index.php
+++ on/doc/index.php
@@ -24,29 +24,29 @@ $content = "
 			<h3 class=\"grey\">{$lang['intro']}</h3>
 			<br />
 			<div style=\"float: left; width: 500px;\">
-				<div style=\"border-left: 3px solid #555555; padding-left: 10px; margin: 0 0 30px 0;\">
+				<div style=\"border-left: 3px solid #d5d5d5; padding-left: 10px; margin: 0 0 30px 0;\">
 					<a href=\"/doc/first\"><h4 class=\"colored\">{$lang['first']}</h4></a>
 					<p>{$lang['first_text']}</p>			
 				</div>
-				<div style=\"border-left: 3px solid #555555; padding-left: 10px; margin: 20px 0 30px 0;\">
+				<div style=\"border-left: 3px solid #d5d5d5; padding-left: 10px; margin: 20px 0 30px 0;\">
 					<a href=\"/doc/concepts\"><h4 class=\"colored\">{$lang['domains']}</h4></a>
 					<p>{$lang['domains_text']}</p>			
 				</div>			
-				<div style=\"border-left: 3px solid #555555; padding-left: 10px; margin: 20px 0 30px 0;\">
+				<div style=\"border-left: 3px solid #d5d5d5; padding-left: 10px; margin: 20px 0 30px 0;\">
 					<a href=\"/doc/databases\"><h4 class=\"colored\">{$lang['databases']}</h4></a>
 					<p>{$lang['databases_text']}</p>			
 				</div>
 			</div>
 			<div style=\"float: right; text-align: left; width: 500px;\">
-				<div style=\"border-left: 3px solid #555555; padding-left: 10px; margin: 0 0 30px 0;\">
+				<div style=\"border-left: 3px solid #d5d5d5; padding-left: 10px; margin: 0 0 30px 0;\">
 					<a href=\"/doc/services\"><h4 class=\"colored\">{$lang['services']}</h4></a>
 					<p>{$lang['services_text']}</p>			
 				</div>
-				<div style=\"border-left: 3px solid #555555; padding-left: 10px; margin: 20px 0 30px 0;\">
+				<div style=\"border-left: 3px solid #d5d5d5; padding-left: 10px; margin: 20px 0 30px 0;\">
 					<a href=\"/developers\"><h4 class=\"colored\">{$lang['developers']}</h4></a>
 					<p>{$lang['developers_text']}</p>			
 				</div>
-				<div style=\"border-left: 3px solid #555555; padding-left: 10px; margin: 20px 0 30px 0;\">
+				<div style=\"border-left: 3px solid #d5d5d5; padding-left: 10px; margin: 20px 0 30px 0;\">
 					<a href=\"https://community.olympe.in\"><h4 class=\"colored\">{$lang['community']}</h4></a>
 					<p>{$lang['community_text']}</p>			
 				</div>
@@ -56,7 +56,7 @@ $content = "
 			<ul style=\"list-style-type: none; margin: 0; padding: 0;\">
 				<div style=\"float: left; width: 500px;\">
 					<li style=\"font-size: 18px; padding: 10px 10px 10px 0;\"><a href=\"/doc/faq/1\">{$lang['question_1']}</a></li>
-					<li style=\"font-size: 18px; padding: 10px 10px 10px 0;\"><a href=\"/doc/domains\">{$lang['question_2']}</a></li>
+					<li style=\"font-size: 18px; padding: 10px 10px 10px 0;\"><a href=\"/doc/info\">{$lang['question_2']}</a></li>
 					<li style=\"font-size: 18px; padding: 10px 10px 10px 0;\"><a href=\"/doc/faq/1\">{$lang['question_3']}</a></li>
 					<li style=\"font-size: 18px; padding: 10px 10px 10px 0;\"><a href=\"/doc/faq/1\">{$lang['question_4']}</a></li>
 					<li style=\"font-size: 18px; padding: 10px 10px 10px 0;\"><a href=\"/doc/faq/1\">{$lang['question_5']}</a></li>

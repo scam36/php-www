667f9a1330113e20724c3c00a5c08964e37d8aa3
Samuel Hassine <samuel.hassine@anotherservice.com>
2013-12-26 03:23:16 +0100

    Too short

--- on/change_password_action.php
+++ on/change_password_action.php
@@ -6,13 +6,20 @@ if( !defined('PROPER_START') )
 	exit;
 }
 
-if( isset($_POST['password']) && $_POST['password'] != $_POST['confirm'] )
-	throw new SiteException("Password mismatch", 400, "Password and confirmation do not match");
+try
+{
+	if( isset($_POST['password']) && $_POST['password'] != $_POST['confirm'] )
+		throw new SiteException("Password mismatch", 400, "Password and confirmation do not match");
 
-$params = array();
-$params['password'] = $_POST['password'];
-		
-api::send('self/update', $params, $_POST['user'] . ':' . $_POST['token']);
+	$params = array();
+	$params['password'] = $_POST['password'];
+
+	api::send('self/update', $params, $_POST['user'] . ':' . $_POST['token']);
+}
+catch(Exception $e)
+{
+	$template->redirect($_SERVER['HTTP_REFERER'] . (strstr($_SERVER['HTTP_REFERER'], 'echangepassword')===false?"?echangepassword":""));
+}
 
 $_SESSION['MESSAGE']['TYPE'] = 'success';
 $_SESSION['MESSAGE']['TEXT']= $lang['message'];

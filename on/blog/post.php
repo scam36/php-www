3a539c5870cc6a991c8657e7238f6d22fc3f79f5
Samuel Hassine <samuel.hassine@anotherservice.com>
2013-12-30 22:00:50 +0100

    Add react

--- on/blog/post.php
+++ on/blog/post.php
@@ -34,6 +34,9 @@ $content = "
 				{$news['content']}
 <!-- FIN ARTICLE -->
 				<br />
+				<a class=\"button classic\" style=\"float: right\" href=\"https://community.olympe.in/category/annonces\">{$lang['react']}</a>
+				<div class=\"clear\"></div>
+				<br />
 			</div>
 ";
 

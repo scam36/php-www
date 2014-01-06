72a80d090d76ade7d4c3b5309db9c2820afd1359
bruno <bruno@ninesys.fr>
2014-01-06 22:43:10 +0100

    pga color

--- on/panel/database.php
+++ on/panel/database.php
@@ -119,7 +119,7 @@ foreach( $databases as $d )
 				<a href=\"#\" target=\"_blank\" style=\"width: 200px; height: 70px; float: left;\">			
 				<img style=\"float: left; margin: 10px 15px 0 0;\" src=\"/{$GLOBALS['CONFIG']['SITE']}/images/phppgadmin.png\" />
 				<span class=\"name\" style=\"margin-top: 25px;\">{$lang['pga']}</span><br />
-				<span class=\"subname\">{$lang['coming_soon']}</span>
+				<span style=\"color:#929292;\">{$lang['coming_soon']}</span>
 				</a>
 			</div>
 	";	

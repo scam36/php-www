394c9a8f5359da43bb233d8539fb6933c23cb129
Samuel Hassine <samuel.hassine@anotherservice.com>
2014-03-27 02:50:04 +0100

    Search by email

--- on/admin/search_action.php
+++ on/admin/search_action.php
@@ -58,16 +58,68 @@ if( $_POST['name'] != $lang['name'] )
 
 		$template->output($content);
 	}	
-	else
+	else if( count($user) == 1 )
 		$template->redirect('/admin/user/detail?id='.$user[0]['id']);
+	else
+		template::redirect('/admin');
+		
 }
 else if( $_POST['email'] != $lang['email'] )
 {
-	$user = api::send('user/select', array('search'=>1, 'email'=>$_POST['email']));
-	if( count($user) == 0 )
-			template::redirect('/admin');
+	$user = api::send('user/select', array('email'=>$_POST['email']));
 
-	$template->redirect('/admin/users/detail?id='.$user[0]['id']);
+	if( count($user) > 1 )
+	{
+		$content = "
+		<div class=\"admin\">
+			<div class=\"top\">
+				<div class=\"left\" style=\"padding-top: 5px;\">
+					<h1 class=\"dark\">{$lang['title']}</h1>
+				</div>
+				<div class=\"right\">
+					
+				</div>
+			</div>
+			<div class=\"clear\"></div><br />
+			<div class=\"container\">
+				<table>
+					<tr>
+						<th style=\"width: 40px; text-align: center;\">#</th>
+						<th>{$lang['name']}</th>
+						<th>{$lang['email']}</th>
+						<th>{$lang['date']}</th>
+						<th>{$lang['ip']}</th>
+						<th  style=\"width: 50px; text-align: center;\">{$lang['actions']}</th>
+					</tr>";
+
+		foreach( $user as $u )
+		{
+			$content .= "
+					<tr>
+						<td style=\"width: 40px; text-align: center;\"><a href=\"/admin/users/detail?id={$u['id']}\"><img style=\"width: 30px; height: 30px;\" src=\"".(file_exists("{$GLOBALS['CONFIG']['SITE']}/images/users/{$u['id']}.png")?"/{$GLOBALS['CONFIG']['SITE']}/images/users/{$u['id']}.png":"/{$GLOBALS['CONFIG']['SITE']}/images/users/user.png")."\" /></a></td>
+						<td>{$u['name']}</td>
+						<td>{$u['email']}</td>
+						<td>".date('Y-m-d', $u['date'])."</td>
+						<td>{$u['ip']}</td>
+						<td style=\"width: 50px; text-align: center;\">
+							<a href=\"/admin/user/detail?id={$u['id']}\" title=\"\"><img class=\"link\" src=\"/{$GLOBALS['CONFIG']['SITE']}/images/icons/large/settings.png\" alt=\"\" /></a>
+						</td>
+					</tr>
+	";
+		}
+	
+		$content .= "
+				</table>
+			</div>
+		</div>
+		";
+
+		$template->output($content);
+	}	
+	else if( count($user) == 1 )
+		$template->redirect('/admin/user/detail?id='.$user[0]['id']);
+	else
+		template::redirect('/admin');
 }
 else if( $_POST['site'] != $lang['site'] )
 {

7671df3db867c591bccb3070a02d2014fa0977be
Samuel Hassine <samuel.hassine@anotherservice.com>
2014-04-03 20:00:59 +0200

    Bugfix

--- on/signup_action.php
+++ on/signup_action.php
@@ -11,7 +11,7 @@ $handle = fopen(__DIR__ . '/banned.txt', 'r');
 if( $handle )
 {
 	while( ($line = fgets($handle)) !== false )
-		$banned[] = $line;
+		$banned[] = trim($line);
 }
 
 if( isset($_POST['antispam']) && $_POST['antispam'] == $_SESSION['ANTISPAM'] && $_POST['conditions'] == 1 )
@@ -21,10 +21,10 @@ if( isset($_POST['antispam']) && $_POST['antispam'] == $_SESSION['ANTISPAM'] && 
 		unset($_SESSION['ANTISPAM']);
 		$_SESSION['JOIN_EMAIL'] = $_POST['email'];
 		$parts = explode('@', $_POST['email']);
-		
-		if( in_array($parts[1], $banned) )
+
+		if( array_search($parts[1], $banned) !== false )
 			throw new SiteException('Invalid or missing arguments', 400, 'Parameter email is on a spammer domain');
-		
+
 		$result = api::send('registration/add', array('auth'=>'', 'email'=>$_POST['email']), $GLOBALS['CONFIG']['API_USERNAME'].':'.$GLOBALS['CONFIG']['API_PASSWORD']);
 
 		$email = str_replace(array('{EMAIL}', '{CODE}', '{DOMAIN}'), array($_POST['email'], $result['code'], $_SERVER["HTTP_HOST"]), $lang['content']);

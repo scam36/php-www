bcaa695cde414182adc906373436cf48bf1b8523
Samuel Hassine <samuel.hassine@anotherservice.com>
2014-04-15 18:40:51 +0200

    Test

--- on/images/sites/index.php
+++ on/images/sites/index.php
@@ -2,9 +2,34 @@
 
 if( $_SERVER["HTTP_HOST"] == 'localhost' || $_SERVER["HTTP_HOST"] == '127.0.0.1' || $_SERVER["HTTP_HOST"] == 'local.olympe.in' )
 	exit();
-	
+
+
+
 $url = str_replace(array('..', '\\', '|', '*', ' ', 'http://'), array('', '', '', '', '', ''), $_GET['url']);
 $file = $url.'.png';
+
+function resize($content)
+{
+	$percent = 0.5;
+	
+	if( $content )
+	{
+		$filename = '/tmp' . md5(time());
+		file_put_contents($filename, $content);
+			
+		list($width, $height) = getimagesize($filename);
+		$newwidth = $width * $percent;
+		$newheight = $height * $percent;
+
+		$thumb = imagecreatetruecolor($newwidth, $newheight);
+		$source = imagecreatefromjpeg($filename);
+
+		$newimage = imagecopyresized($thumb, $source, 0, 0, 0, 0, $newwidth, $newheight, $width, $height);
+		unlink($filename);
+	}
+	
+	return $newimage;
+}
 
 if( file_exists($file) )
 {
@@ -14,22 +39,20 @@ if( file_exists($file) )
 	
 	if( $mod <= $current-(3600*24*30) || $size < 10 )
 	{
-		$address = 'http://172.16.1.200:3000?width=200&height=150&url=' . $url . '&clipRect={"top":0,"left":0,"width":800,"height":600}';
+		$address = 'http://172.16.1.200:3000?url=' . $url . '&clipRect={"top":0,"left":0,"width":1024,"height":600}';
 		$content = file_get_contents($address);
-		if( $content )
-			file_put_contents($file, $content);
-		else
-			file_put_contents($file, file_get_contents('site.png'));
+		$content = resize($content);
+		
+		file_put_contents($file, $content);
 	}
 }
 else
 {
-	$address = 'http://172.16.1.200:3000?width=200&height=150&url=' . $url . '&clipRect={"top":0,"left":0,"width":800,"height":600}';
+	$address = 'http://172.16.1.200:3000?url=' . $url . '&clipRect={"top":0,"left":0,"width":1024,"height":600}';
 	$content = file_get_contents($address);
-	if( $content )
-		file_put_contents($file, $content);
-	else
-		file_put_contents($file, file_get_contents('site.png'));
+	$content = resize($content);
+		
+	file_put_contents($file, $content);
 }
 
 header("content-type: image/png");

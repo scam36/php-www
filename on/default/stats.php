f5733fc86304844c82bd621e6b0084bd1eae9967
Samuel Hassine <samuel.hassine@anotherservice.com>
2014-05-11 04:40:40 +0200

    NUmber FR

--- on/default/stats.php
+++ on/default/stats.php
@@ -11,13 +11,28 @@ $sites = api::send('site/list', array('count'=>1), $GLOBALS['CONFIG']['API_USERN
 $dbs = api::send('database/list', array('count'=>1), $GLOBALS['CONFIG']['API_USERNAME'].':'.$GLOBALS['CONFIG']['API_PASSWORD']);
 $domains = api::send('domain/list', array('count'=>1), $GLOBALS['CONFIG']['API_USERNAME'].':'.$GLOBALS['CONFIG']['API_PASSWORD']);
 
+switch( translator::getLanguage() )
+{
+	case 'FR':
+		$users['count'] = number_format($users['count'], 0, ',', ' ');
+		$sites['count'] = number_format($sites['count'], 0, ',', ' ');
+		$dbs['count'] = number_format($dbs['count'], 0, ',', ' ');
+		$domains['count'] = number_format($domains['count'], 0, ',', ' ');
+	break;
+	default:
+		$users['count'] = number_format($users['count']);
+		$sites['count'] = number_format($sites['count']);
+		$dbs['count'] = number_format($dbs['count']);
+		$domains['count'] = number_format($domains['count']);
+}
+
 $content = "		
-			<span style=\"display: block; font-size: 70px; margin: 0 auto;\">".number_format($users['count'])."</span>
+			<span style=\"display: block; font-size: 70px; margin: 0 auto;\">{$users['count']}</span>
 			<span style=\"display: block; font-size: 30px; color: #7bbb51; margin-top: 5px;\">{$lang['users']}</span>
 			<span style=\"display: block; font-size: 18px; margin-top: 20px;\">
-				<span style=\"color: #910000\">".number_format($sites['count'])."</span> {$lang['sites']}, 
-				<span style=\"color: #910000\">".number_format($dbs['count'])."</span> {$lang['databases']} 
-				<span style=\"color: #910000\">".number_format($domains['count'])."</span> {$lang['domains']}
+				<span style=\"color: #910000\">{$sites['count']}</span> {$lang['sites']}, 
+				<span style=\"color: #910000\">{$dbs['count']}</span> {$lang['databases']} 
+				<span style=\"color: #910000\">{$domains['count']}</span> {$lang['domains']}
 			</span>
 ";
 
